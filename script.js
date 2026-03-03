87cd18973825ffcf4fd37b8f641dcf0ae3a64352
fantom211 <128746008+fantom211@users.noreply.github.com>
2026-03-03 17:39:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
-$('#block').css({
-    'background-color':'red', width:'200px', height:'200px'
+symbols = new Array("🎈","🎨","🧶","💎","🔮");
+
+let cards = $('.game-section .card').toArray();
+
+for(i=0; i<5;i++)
+{
+    $(cards[i]).text(symbols[i]);
+    $(cards[i+5]).text(symbols[i]);
+}
+
+cards.sort(()=>Math.random()-0.5);
+$('.game-section').empty().append(cards);
+
+$('#play-button').on('click', function(){
+    if ($(this).text() == 'START') {
+        $('.card').removeClass('card-face matched').addClass('card-back');
+        $(this).text('FINISH');
+    } else {
+        $('.card').removeClass('card-back matched').addClass('card-face');
+        $('.card').off('click').css('pointer-events', 'none');
+        $(this).text('START').prop('disabled', false);
+    }
+});
+
+
+let counter=0;
+
+
+cards.forEach((card)=>{
+    $(card).on('click', function(){
+        if($(this).hasClass('card-face') || $(this).hasClass('matched')) return;
+
+        counter++;
+        $(this).addClass('card-face');
+
+        if(counter==2){
+            currentOpenCards=$('.card-face:not(.matched)');
+            if(currentOpenCards.length==2){
+                if($(currentOpenCards[0]).text()==$(currentOpenCards[1]).text())
+                {
+                    $(currentOpenCards[0]).addClass('matched');
+                    $(currentOpenCards[1]).addClass('matched');                    
+                    console.log("Пара найдена");
+                    counter=0;
+                }
+                else{
+                    console.log("Не пара");
+                    setTimeout(()=>{
+                        currentOpenCards.removeClass('card-face');
+                        counter=0;
+                        
+                    },1000);
+                }
+            }
+        }
+    })
 })
+
+
